4b61f0e43b54dad1590328823d7ce0f315b76cf7
diyaxbudhia <diya.budhia@gmail.com>
2024-08-15 23:34:16 +0200

    task 1 edit

--- get_user_choice.asm
+++ get_user_choice.asm
@@ -4,6 +4,9 @@
 ; Group member 03: Alisha_Perumal_u22512285
 ; ==========================
 
+section .data 
+    message db "Choice: "
+
 section .bss
     ; ==========================
     ; Your data goes here
@@ -17,6 +20,13 @@ extern greeting
 get_user_choice:
     ; Call the greeting function to print the welcome message
     call greeting
+    
+    
+    mov rax, 1                ; syscall number for sys_write
+    mov rdi, 1                ; file descriptor 1 is stdout
+    mov rsi, message          ; address of the string
+    mov rdx, 8                ; length of the string ("Choice: " + newline)
+    syscall
 
     ; ==========================
     ; Your data goes here
--- greeting.asm
+++ greeting.asm
@@ -6,23 +6,23 @@
 
 section .data
     ; ==========================
-    message db "Welcome agent. What do you want to do, Encrypt[1] or Decrypt[2]?"
+    message db "Welcome agent. What do you want to do, Encrypt[1] or Decrypt[2]?", 0x0a, 0
     ; ==========================
 
 section .text
     global greeting
 
 greeting:
-    ; Do not modify anything above this line unless you know what you are doing
-    ; ==========================
     
-    mov rax, 1
-    mov rdi, 1
-    mov rsi, message
-    mov rdx, 64
-    syscall
+    ; ==========================
+
+    mov rax, 1                
+    mov rdi, 1                
+    mov rsi, message          
+    mov rdx, 65               
+    syscall                   
 
     ; ==========================
     ; Do not modify anything below this line unless you know what you are doing
 
-    ret                          ;return from function
+    ret                       ; return from function
