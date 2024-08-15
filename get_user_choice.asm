1e9d941cf30802e485e985df8f8494c3a67af1ef
Sammy-2903 <samvitprakash2004@gmail.com>
2024-08-15 23:37:14 +0200

    Update

--- get_user_choice.asm
+++ get_user_choice.asm
@@ -25,8 +25,6 @@ get_user_choice:
     mov rsi, choice
     call input
 
-
-    
     ; ==========================
     ; Do not modify anything below this line unless you know what you are doing 
     ret
