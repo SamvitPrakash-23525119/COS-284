7b2e335e3fd565a8ff09c7678b102a2c73d2fd78
alishaperumal <alishaperumal11@gmail.com>
2024-08-29 18:11:41 +0200

    added name

--- Practical 2/convert_string_to_float.asm
+++ Practical 2/convert_string_to_float.asm
@@ -1,7 +1,7 @@
 ; ==========================
 ; Group member 01: Name_Surname_student-nr
 ; Group member 02: Name_Surname_student-nr
-; Group member 03: Name_Surname_student-nr
+; Group member 03: Alisha_Perumal_u22512285
 ; ==========================
 section .data
     global convertStringToFloat
