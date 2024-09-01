a46ad140711ce14b2fcc1709f958aaa0e8eabb90
alishaperumal <alishaperumal11@gmail.com>
2024-09-01 20:32:07 +0200

    task 1 completed

--- Practical 2/convert_string_to_float.asm
+++ Practical 2/convert_string_to_float.asm
@@ -4,6 +4,31 @@
 ; Group member 03: Alisha_Perumal_u22512285
 ; ==========================
 section .data
+    errno db 0                     
+    extern strtof                   
+
+section .bss
+    endptr resq 1                   
+
+section .text
     global convertStringToFloat
 
 convertStringToFloat:
+    mov byte [errno], 0
+
+    ; Prepare for the strtof call
+    lea rsi, [endptr]            
+    call strtof                     
+
+    cmp byte [errno], 0
+    jne .conversion_error           
+
+    mov rax, [endptr]               
+    cmp byte [rax], 0               
+    jne .conversion_error           
+
+    ret
+
+.conversion_error:
+    xorps xmm0, xmm0              
+    ret
