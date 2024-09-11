5e51bf76d7592752ad7a9457faea00ae21196b2d
Sammy-2903 <samvitprakash2004@gmail.com>
2024-09-12 00:56:45 +0200

    Completion of Task 1

---
+++ Practical 3/multiply_scalar_to_matrix.asm
@@ -0,0 +1,44 @@
+; ==========================
+; Group member 01: Samvit_Prakash_u23525119
+; Group member 02: Name_Surname_student-nr
+; Group member 03: Name_Surname_student-nr
+; ==========================
+segment .text
+        global multiplyScalarToMatrix
+        
+multiplyScalarToMatrix:   
+
+        xor r10, r10                    ; Setting Counter Variable to 0
+        xor r11, r11                    ; Setting Counter Variable to 0
+
+loop_row_begin:                         ; Start of Outer For Loop
+        
+        cmp r10, rsi                    ; Checking if r10 < rsi [rsi is the upper bound of the loop]
+        jge loop_row_end                ; Goto end of outer loop if r10 >= rsi
+
+loop_col_begin:                         ; Start of Inner Loop
+        
+        cmp r11, rdx                    ; Checking if r11 < rdx [rdx is the upper bound of the loop]
+        jge loop_col_end                ; Goto end of inner loop if r11 >= rdx
+
+        mov rax, [rdi+8*r10]            ; Setting rax to matrix[y] [Elements are 8 bytes] 
+
+        movss xmm1, [rax+4*r11]         ; Setting xmm1 to matrix[y][x] [Elements are 4 bytes]                   
+
+        mulss xmm1, xmm0                ; Multiplying matrix[y][x] by scalar [Resultant stored in xmm1]
+
+        movss [rax+4*r11], xmm1         ; Setting matrix[y][x] to xmm1 [Resultant of scalar multiplication]
+
+
+        inc r11                         ; Increasing the counter variable by 1
+        jmp loop_col_begin              ; Goto start of inner loop
+
+        loop_col_end:                   ; End of inner loop
+                
+        xor r11, r11                    ; Resetting the counter variable to 0 to prepare for next loop
+        inc r10                         ; Increasing the counter variable by 1
+        jmp loop_row_begin              ; Goto start of outer loop
+        
+loop_row_end:                           ; End of outer loop
+
+        ret                             ; return
